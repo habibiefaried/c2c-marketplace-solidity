161168729dcbce76023bc33aa4c76dcca772da41
Habibie Faried <habibiefaried@gmail.com>
2022-01-30 10:15:07 +0400

    feat: store

--- contracts/C2CMarketCoin.sol
+++ contracts/C2CMarketCoin.sol
@@ -1,15 +1,20 @@
 // SPDX-License-Identifier: MIT
 // For demonstration, 1 eth is equal to 1000 C2CMarketCoin
+// 1 user is ONLY able to handle 1 store
 
 pragma solidity ^0.8.0;
 
 import "../contracts/C2CMarketCoinERC20.sol";
+import "../contracts/C2CMarketCoinStore.sol";
 
 contract C2CMarketCoin is C2CMarketCoinERC20 {
 	address payable _contractOwner;
+	mapping(address => C2CMarketCoinStore) private _allstores;
+	mapping(address => bool) private _allinitializedstore;
 
 	event Deposit(address indexed sender, uint256 amount, uint256 currentBalance);
 	event Withdraw(address indexed to, uint256 amount, uint256 currentBalance);
+	event SetItem(address indexed from, string name, uint256 price, uint256 qty, string imagelink);
 
 	modifier onlyOwner() {
         require(msg.sender == _contractOwner, "not owner");
@@ -25,13 +30,24 @@ contract C2CMarketCoin is C2CMarketCoinERC20 {
 	}
 
 	function mint() external payable {
+		require(msg.value > 0, "invalid amount of money");
 		uint256 money = msg.value * 1000;
-
 		_balances[msg.sender] += money;
 		_totalSupply += money;
+
+		if (_allinitializedstore[msg.sender] == false){
+			_allinitializedstore[msg.sender] = true;
+			_allstores[msg.sender] = C2CMarketCoinStore(msg.sender);
+		}
 		emit Deposit(msg.sender, msg.value, address(this).balance);
 	}
 
+	function setItem(string memory _itemname, uint256 _itemprice, uint256 _qty, string memory _imagelink) public {
+		_allstores[msg.sender].setItem(_itemname, _itemprice, _qty, _imagelink);
+		emit SetItem(msg.sender, _itemname, _itemprice, _qty, _imagelink);
+	}
+
+	// Related to ETH
 	function getContractBalanceETH() external view returns (uint256) {
         return address(this).balance;
     }
---
+++ contracts/C2CMarketCoinStore.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+// This contract will store all listing they are selling
+
+pragma solidity ^0.8.0;
+
+contract C2CMarketCoinStore {
+	address payable _storeOwner;
+	struct Item { 
+	  uint256 price; // in C2CMarketCoin
+	  uint256 qty; // quantity, number of item this have
+	  string imagelink; //maybe image link to aws s3 or ipfs protocol
+	  bool isExist; // for checking purpose if item is exist
+   	}
+
+   	string[] private _myitemkeys; //storing item keys for below
+   	mapping(string => Item) private _myitems;
+
+   	constructor(address _owner) {
+		_storeOwner = payable(_owner);
+    }
+
+   	function setItem(string memory _name, uint256 _price, uint256 _qty, string memory _imagelink) public {
+   		if (_myitems[_name].isExist == false){
+   			_myitemkeys.push(_name);
+   		}
+
+   		_myitems[_name] = Item({price: _price, qty: _qty, imagelink: _imagelink, isExist: true});
+   	}
+
+   	function getOneItem(string memory _name) public view returns (uint256, uint256, string memory) {
+   		return (_myitems[_name].price, _myitems[_name].qty, _myitems[_name].imagelink);
+   	}
+
+   	function getAllItems() public view returns (string[] memory){
+   		return _myitemkeys;
+   	}
+}
