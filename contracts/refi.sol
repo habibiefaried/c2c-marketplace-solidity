b111ce0a9d486c1b192f1797bdc84924e1b347b5
Habibie Faried <habibiefaried@gmail.com>
2022-01-31 20:55:33 +0400

    feat: versi

--- contracts/refi.sol
+++ contracts/refi.sol
@@ -6,7 +6,7 @@
 
 // hevm: flattened sources of src/ReFi.sol
 // SPDX-License-Identifier: MIT
-pragma solidity =0.8.10 >=0.8.0 <0.9.0;
+pragma solidity ^0.8.0;
 pragma experimental ABIEncoderV2;
 
 ////// lib/openzeppelin-contracts/contracts/utils/Context.sol
